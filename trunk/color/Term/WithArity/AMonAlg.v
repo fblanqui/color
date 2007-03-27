f04ea8fcc0c19e74452a341cc9b349a0fba1c482
koper <koper@b0d5273c-1889-4eb6-95f8-211497179ee6>
2007-03-27 15:33:01 +0000

    Monotone algebras instantiated to matrices resulting in matrix interpretations for relative, top termination (more to come). WARNING: For the time being 3 lemmas in VecUtil are Admitted (to be proven soon).

--- trunk/color/Term/WithArity/AMonAlg.v
+++ trunk/color/Term/WithArity/AMonAlg.v
@@ -13,35 +13,35 @@ References:
    Conference (IJCAR 2006), 2006.
 *)
 
-Require Import AWFMInterpretation.
+Require Export AWFMInterpretation.
 
 (***********************************************************************)
 (** * Module type specifying a weakly monotone algebra.                *)
 (***********************************************************************)
 Module Type MonotoneAlgebraType.
 
-  Parameter Sig: Signature.
+  Parameter Sig : Signature.
     
   (**
      A weakly monotone algebra consist of:
      - interpretation [I]
-     - [gt] and [ge] relations
+     - [succ] and [succeq] relations
      along with the following conditions:
-     - [IM]: [I] is monotone with respect to [gt]
-     - [gt_wf]: [gt] is well-founded
-     - [gt_ge_compat]: compatibility between [gt] and [ge]
+     - [IM]: [I] is monotone with respect to [succ]
+     - [succ_wf]: [succ] is well-founded
+     - [succ_succeq_compat]: compatibility between [succ] and [succeq]
   *)
-  Parameter I: interpretation Sig.
+  Parameter I : interpretation Sig.
   
-  Parameter gt: relation (domain I).
+  Parameter succ : relation (domain I).
   
-  Parameter ge: relation (domain I).
+  Parameter succeq : relation (domain I).
   
-  Parameter monotone_ge: monotone I ge.
+  Parameter monotone_succeq : monotone I succeq.
 
-  Parameter gt_wf: WF gt.
+  Parameter succ_wf : WF succ.
 
-  Parameter gt_ge_compat: absorb gt ge.
+  Parameter succ_succeq_compat : absorb succ succeq.
 
 End MonotoneAlgebraType.
 
@@ -50,12 +50,12 @@ End MonotoneAlgebraType.
 (***********************************************************************)
 Module Type ExtendedMonotoneAlgebraType.
 
-  Declare Module MA: MonotoneAlgebraType.
+  Declare Module MA : MonotoneAlgebraType.
   Export MA.
 
   (** Extended monotone algebra is a wekaly monotone algebra in which every operation
-      is monotone with respect to [gt] *)
-  Parameter monotone_gt: monotone I gt.
+      is monotone with respect to [succ] *)
+  Parameter monotone_succ : monotone I succ.
 
 End ExtendedMonotoneAlgebraType.
 
@@ -67,18 +67,18 @@ Module MonotoneAlgebraResults (MA : MonotoneAlgebraType).
   Export MA.
   Require Import ACompat.
 
-  Definition IR_gt := IR I gt.
-  Definition IR_ge := IR I ge.
+  Definition IR_succ := IR I succ.
+  Definition IR_succeq := IR I succeq.
 
   Notation term := (term Sig).
   Notation rule := (rule Sig).
   Notation rules := (list rule).
 
-  Lemma absorb_gt_ge : absorb IR_gt IR_ge.
+  Lemma absorb_succ_succeq : absorb IR_succ IR_succeq.
 
   Proof.
     intros x z xz val.
-    apply gt_ge_compat.
+    apply succ_succeq_compat.
     destruct xz as [y [ge_xy gt_yz]].
     exists (term_int val y). split; auto.
   Qed.
@@ -92,11 +92,11 @@ Module MonotoneAlgebraResults (MA : MonotoneAlgebraType).
     assumption.
   Qed.
 
-  Lemma ma_ge_rewrite_ordering : rewrite_ordering IR_ge.
+  Lemma ma_succeq_rewrite_ordering : rewrite_ordering IR_succeq.
 
   Proof.
-    unfold IR_ge. apply IR_rewrite_ordering.
-    exact monotone_ge.
+    unfold IR_succeq. apply IR_rewrite_ordering.
+    exact monotone_succeq.
   Qed.
 
 (**********************************************************************)
@@ -106,17 +106,17 @@ Module MonotoneAlgebraResults (MA : MonotoneAlgebraType).
     Variable R E : rules.
 
     Lemma ma_compat_hd_red_mod : 
-      compatible IR_gt R ->
-      compatible IR_ge E ->
+      compatible IR_succ R ->
+      compatible IR_succeq E ->
       WF (hd_red_mod E R).
 
     Proof.
-      intros. apply WF_incl with IR_gt.
-      apply compat_hd_red_mod with IR_ge; trivial.
-      unfold IR_gt. apply IR_substitution_closed.
-      exact ma_ge_rewrite_ordering.
-      exact absorb_gt_ge.
-      unfold IR_gt. apply IR_WF. exact gt_wf.
+      intros. apply WF_incl with IR_succ.
+      apply compat_hd_red_mod with IR_succeq; trivial.
+      unfold IR_succ. apply IR_substitution_closed.
+      exact ma_succeq_rewrite_ordering.
+      exact absorb_succ_succeq.
+      unfold IR_succ. apply IR_WF. exact succ_wf.
     Qed.
 
   End TopTermination.
@@ -131,8 +131,8 @@ Module MonotoneAlgebraResults (MA : MonotoneAlgebraType).
     (* Modular removal of rules *)
     Lemma ma_modular_hd_red_mod :
       WF (hd_red_mod E R') ->
-      compatible IR_gt R ->
-      compatible IR_ge (R' ++ E) ->
+      compatible IR_succ R ->
+      compatible IR_succeq (R' ++ E) ->
       WF (hd_red_mod E (R ++ R')).
 
     Proof.
@@ -155,7 +155,7 @@ End MonotoneAlgebraResults.
 (***********************************************************************)
 (** * Functor with a theory of extended monotone algebras              *)
 (***********************************************************************)
-Module ExtendedMonotoneAlgebraResults (EMA: ExtendedMonotoneAlgebraType).
+Module ExtendedMonotoneAlgebraResults (EMA : ExtendedMonotoneAlgebraType).
 
   Module MAR := MonotoneAlgebraResults EMA.MA.
   Export MAR.
@@ -163,11 +163,11 @@ Module ExtendedMonotoneAlgebraResults (EMA: ExtendedMonotoneAlgebraType).
 
   Require Import ACompat.
 
-  Lemma ma_gt_rewrite_ordering : rewrite_ordering IR_gt.
+  Lemma ma_succ_rewrite_ordering : rewrite_ordering IR_succ.
 
   Proof.
-    unfold IR_gt. apply IR_rewrite_ordering.
-    exact monotone_gt.
+    unfold IR_succ. apply IR_rewrite_ordering.
+    exact monotone_succ.
   Qed.
 
 (**********************************************************)
@@ -178,17 +178,17 @@ Module ExtendedMonotoneAlgebraResults (EMA: ExtendedMonotoneAlgebraType).
 
     (* Termination in one go *)
     Lemma ma_compat_red_mod : 
-      compatible IR_gt R ->
-      compatible IR_ge E ->
+      compatible IR_succ R ->
+      compatible IR_succeq E ->
       WF (red_mod E R).
 
     Proof.
-      intros. apply WF_incl with IR_gt.
-      apply compat_red_mod with IR_ge; trivial.
-      exact ma_gt_rewrite_ordering.
-      exact ma_ge_rewrite_ordering.
-      exact absorb_gt_ge.
-      unfold IR_gt. apply IR_WF. exact gt_wf.
+      intros. apply WF_incl with IR_succ.
+      apply compat_red_mod with IR_succeq; trivial.
+      exact ma_succ_rewrite_ordering.
+      exact ma_succeq_rewrite_ordering.
+      exact absorb_succ_succeq.
+      unfold IR_succ. apply IR_WF. exact succ_wf.
     Qed.
 
   End RelativeTermination.
@@ -203,8 +203,8 @@ Module ExtendedMonotoneAlgebraResults (EMA: ExtendedMonotoneAlgebraType).
     (* Modular removal of rules *)
     Lemma ma_modular_red_mod :
       WF (red_mod E' R') ->
-      compatible IR_gt (R  ++ E ) ->
-      compatible IR_ge (R' ++ E') ->
+      compatible IR_succ (R  ++ E ) ->
+      compatible IR_succeq (R' ++ E') ->
       WF (red_mod (E ++ E') (R ++ R')).
 
     Proof.
