793efe0d8d09ef0adcb0c2ad0a9ef04053209892
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2009-01-21 01:01:11 +0000

    detail

--- trunk/color/Util/Relation/NotSN_IS.v
+++ trunk/color/Util/Relation/NotSN_IS.v
@@ -7,7 +7,7 @@ See the COPYRIGHTS and LICENSE files.
 ~SN terms give infinite sequences (using classical logic and dependent choice)
 *)
 
-(* $Id: NotSN_IS.v,v 1.2 2008-05-14 12:26:56 blanqui Exp $ *)
+(* $Id: NotSN_IS.v,v 1.3 2009-01-21 01:01:11 blanqui Exp $ *)
 
 Set Implicit Arguments.
 
@@ -21,8 +21,8 @@ Variables (A : Type) (R : relation A) (a : A) (h : ~SN R a).
 Lemma notSN_IS : exists f, IS R f.
 
 Proof.
-set (P := fun x => ~SN R x). set (B := sig P). set (T := Rof R (@proj1_sig A P)).
-assert (forall x, exists y, T x y).
+set (P := fun x => ~SN R x). set (B := sig P).
+set (T := Rof R (@proj1_sig A P)). assert (forall x, exists y, T x y).
 intro. destruct x. unfold T. simpl. ded (notSN_succ p). decomp H.
 exists (exist P x0 H2). simpl. exact H1.
 set (b := exist P a h). ded (@dep_choice B b T H). destruct H0 as [g].
