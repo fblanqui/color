2c02697a4f15a4914310daaf0f47f0efa161ef44
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2009-05-07 09:50:07 +0000

    added tactic for proving beq_symb_ok

--- Term/WithArity/ASignature.v
+++ Term/WithArity/ASignature.v
@@ -9,10 +9,12 @@ signature for algebraic terms with arity
 
 Set Implicit Arguments.
 
+(***********************************************************************)
 (** Variables are represented by natural numbers. *)
 
 Notation variable := nat (only parsing).
 
+(***********************************************************************)
 (** Signature with a decidable set of symbols of fixed arity. *)
 
 Record Signature : Type := mkSignature {
@@ -32,3 +34,10 @@ Require Import EqUtil.
 Definition eq_symb_dec Sig := dec_beq (@beq_symb_ok Sig).
 
 Implicit Arguments eq_symb_dec [Sig].
+
+(***********************************************************************)
+(** Tactic for proving beq_symb_ok *)
+
+Ltac beq_symb_ok_tac := intros f g; split;
+  [destruct f; destruct g; simpl; intro; (discriminate || reflexivity)
+    | intro; subst g; destruct f; reflexivity].
