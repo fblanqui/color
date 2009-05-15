08eca9417cfad19f11811e25da5968a110f96a33
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2009-05-15 05:29:22 +0000

    added preservation of termination by isomorphism

--- Term/WithArity/AMorphism.v
+++ Term/WithArity/AMorphism.v
@@ -18,11 +18,15 @@ Require Import SN.
 
 Section Morphism.
 
-Variable S1 S2 : Signature.
+(***********************************************************************)
+(** we assume given a morphism between two signatures S1 and S2 *)
 
-Variable F : S1 -> S2.
+Variables S1 S2 : Signature.
 
-Variable hyp1 : forall f, arity f = arity (F f).
+Variables (F : S1 -> S2) (HF : forall f, arity f = arity (F f)).
+
+(***********************************************************************)
+(** translation of terms in S1 into terms in S2 *)
 
 Fixpoint Ft (t : term S1) : term S2 :=
   match t with
@@ -33,17 +37,20 @@ Fixpoint Ft (t : term S1) : term S2 :=
           | Vnil => Vnil
           | Vcons t n' ts' => Vcons (Ft t) (Fv n' ts')
         end
-        in Fun (F f) (Vcast (Fv (arity f) ts) (hyp1 f))
+        in Fun (F f) (Vcast (Fv (arity f) ts) (HF f))
   end.
 
 Definition Fv := Vmap Ft.
 
 Lemma Ft_fun : forall f ts,
-  Ft (Fun f ts) = Fun (F f) (Vcast (Fv ts) (hyp1 f)).
+  Ft (Fun f ts) = Fun (F f) (Vcast (Fv ts) (HF f)).
 
 Proof.
 intros. simpl. apply args_eq. apply Vcast_eq. refl.
 Qed.
+
+(***********************************************************************)
+(** translation of contexts in S1 into contexts in S2 *)
 
 Lemma Fc_aux : forall i j n n', i+S j=n -> n=n' -> i+S j=n'.
 
@@ -57,7 +64,7 @@ Fixpoint Fc (c : context S1) : context S2 :=
   match c with
     | Hole => Hole
     | Cont f _ _ e v1 c' v2 =>
-      Cont (F f) (Fc_aux e (hyp1 f)) (Fv v1) (Fc c') (Fv v2)
+      Cont (F f) (Fc_aux e (HF f)) (Fv v1) (Fc c') (Fv v2)
   end.
 
 Lemma Ft_fill : forall t c, Ft (fill c t) = fill (Fc c) (Ft t).
@@ -67,10 +74,16 @@ induction c; intros. refl. simpl fill. rewrite Ft_fun. apply args_eq.
 unfold Fv. rewrite Vmap_cast. rewrite Vmap_app. simpl. rewrite Vcast_cast.
 rewrite IHc. apply Vcast_pi.
 Qed.
+
+(***********************************************************************)
+(* translation of rules in S1 into rules in S2 *)
  
 Definition Fr (a : rule S1) := let (l,r) := a in mkRule (Ft l) (Ft r).
 
-Notation Frs := (List.map Fr).
+Definition Frs := List.map Fr.
+
+(***********************************************************************)
+(* translation of substitutions in S1 into substitutions in S2 *)
 
 Definition Fs (s : substitution S1) x := Ft (s x).
 
@@ -83,6 +96,9 @@ refl. rewrite Ft_fun. repeat rewrite sub_fun. rewrite Ft_fun. apply args_eq.
 rewrite H. rewrite Vmap_cast. refl.
 refl. simpl. rewrite H. rewrite H0. refl.
 Qed.
+
+(***********************************************************************)
+(** F preserves rewriting *)
 
 Lemma Fred : forall R t u, red R t u -> red (Frs R) (Ft t) (Ft u).
 
@@ -126,6 +142,9 @@ intros. redtac. unfold hd_red_mod. exists (Ft t0). split.
 apply Fred_rtc. hyp. apply Fhd_red. subst. apply hd_red_rule. hyp.
 Qed.
 
+(***********************************************************************)
+(** reflexion of termination *)
+
 Ltac geneq H x e := generalize (refl_equal e); generalize (H e);
   clear H; generalize e at -2; intros t h; gen x; gen h; gen t.
 
@@ -154,3 +173,154 @@ eapply H0. subst x. apply Fhd_red_mod. apply H2. refl.
 Qed.
 
 End Morphism.
+
+Implicit Arguments Ft [S1 S2 F].
+Implicit Arguments Fv [S1 S2 F n].
+Implicit Arguments Fc [S1 S2 F].
+Implicit Arguments Fs [S1 S2 F].
+Implicit Arguments Fr [S1 S2 F].
+Implicit Arguments Frs [S1 S2 F].
+
+(***********************************************************************)
+(** preservation of termination *)
+
+Section Preserv.
+
+Variables S1 S2 : Signature.
+Variables (F : S1 -> S2) (HF : forall f, arity f = arity (F f)).
+Variables (G : S2 -> S1) (HG : forall f, arity f = arity (G f)).
+Variables (E R : rules S1) (E' R' : rules S2).
+Variables (hyp1 : incl (Frs HG E') E) (hyp2 : incl (Frs HF E) E').
+Variables (hyp3 : incl (Frs HG R') R) (hyp4 : incl (Frs HF R) R').
+
+Lemma WF_Fred : WF (red R) -> WF (red (Frs HF R)).
+
+Proof.
+intro. eapply WF_incl. apply red_incl. apply hyp4.
+eapply Fred_WF. eapply WF_incl. apply red_incl. apply hyp3. hyp.
+Qed.
+
+Lemma WF_Fred_mod : WF (red_mod E R) -> WF (red_mod (Frs HF E) (Frs HF R)).
+
+Proof.
+intro. eapply WF_incl. apply red_mod_incl. apply hyp4. apply hyp2.
+eapply Fred_mod_WF. eapply WF_incl. apply red_mod_incl. apply hyp3.
+apply hyp1. hyp.
+Qed.
+
+Lemma WF_Fhd_red_mod :
+  WF (hd_red_mod E R) -> WF (hd_red_mod (Frs HF E) (Frs HF R)).
+
+Proof.
+intro. eapply WF_incl. apply hd_red_mod_incl. apply hyp2. apply hyp4.
+eapply Fhd_red_mod_WF. eapply WF_incl. apply hd_red_mod_incl. apply hyp1.
+apply hyp3. hyp.
+Qed.
+
+End Preserv.
+
+(***********************************************************************)
+(** epimorphism *)
+
+Section Epi.
+
+Variables S1 S2 : Signature.
+Variables (F : S1 -> S2) (HF : forall f, arity f = arity (F f)).
+Variables (G : S2 -> S1) (HG : forall f, arity f = arity (G f)).
+Variable FG : forall f, F (G f) = f.
+
+Lemma HG_epi : forall f, arity f = arity (G f).
+
+Proof.
+intro. rewrite <- (FG f) at 1. symmetry. apply HF.
+Qed.
+
+Lemma Ft_epi : forall t, Ft HF (Ft HG t) = t.
+
+Proof.
+intro t. pattern t; apply term_ind with
+  (Q := fun n (ts : vector (term S2) n) => Fv HF (Fv HG ts) = ts); clear t.
+(* Var *)
+refl.
+(* Fun *)
+intros. repeat rewrite Ft_fun. eapply fun_eq_intro with (h:=FG f).
+unfold Fv in *. rewrite Vmap_cast. repeat rewrite Vcast_cast. rewrite H.
+symmetry. apply Vcast_refl.
+(* Vnil *)
+refl.
+(* Vcons *)
+intros. simpl. rewrite H. rewrite H0. refl.
+Qed.
+
+Lemma Fv_epi : forall n (ts : vector (term S2) n), Fv HF (Fv HG ts) = ts.
+
+Proof.
+induction ts; simpl; intros. refl. rewrite IHts. rewrite Ft_epi. refl.
+Qed.
+
+Lemma Fc_epi : forall c, Fc HF (Fc HG c) = c.
+
+Proof.
+induction c; simpl; intros. refl. eapply cont_eq_intro
+  with (h1 := refl_equal i) (h2 := refl_equal j). apply FG. hyp.
+rewrite Fv_epi. rewrite Vcast_refl. refl.
+rewrite Fv_epi. rewrite Vcast_refl. refl.
+Qed.
+
+Lemma Fr_epi : forall a, Fr HF (Fr HG a) = a.
+
+Proof.
+destruct a as [l r]. simpl. repeat rewrite Ft_epi. refl.
+Qed.
+
+Lemma Frs_epi : forall l, Frs HF (Frs HG l) = l.
+
+Proof.
+induction l; simpl; intros. refl. rewrite Fr_epi. rewrite IHl. refl.
+Qed.
+
+End Epi.
+
+Implicit Arguments HG_epi [S1 S2 F G].
+
+(***********************************************************************)
+(** isomorphism *)
+
+Section Iso.
+
+Variables S1 S2 : Signature.
+Variables (F : S1 -> S2) (HF : forall f, arity f = arity (F f)).
+Variables (G : S2 -> S1) (HG : forall f, arity f = arity (G f)).
+Variables (FG : forall f, F (G f) = f) (GF : forall f, G (F f) = f).
+Variables E R : rules S1.
+
+Lemma WF_Fred_iso : WF (red R) <-> WF (red (Frs HF R)).
+
+Proof.
+split; intro. apply WF_Fred with (G:=G) (HG:=HG) (R':=Frs HF R).
+rewrite Frs_epi. apply incl_refl. apply GF. apply incl_refl. hyp.
+apply Fred_WF with (S2:=S2) (F:=F) (HF:=HF). hyp.
+Qed.
+
+Lemma WF_Fred_mod_iso : WF (red_mod E R) <-> WF (red_mod (Frs HF E) (Frs HF R)).
+
+Proof.
+split; intro.
+apply WF_Fred_mod with (G:=G) (HG:=HG) (R':=Frs HF R) (E':=Frs HF E).
+rewrite Frs_epi. apply incl_refl. apply GF. apply incl_refl.
+rewrite Frs_epi. apply incl_refl. apply GF. apply incl_refl. hyp.
+apply Fred_mod_WF with (S2:=S2) (F:=F) (HF:=HF). hyp.
+Qed.
+
+Lemma WF_Fhd_red_mod_iso :
+  WF (hd_red_mod E R) <-> WF (hd_red_mod (Frs HF E) (Frs HF R)).
+
+Proof.
+split; intro.
+apply WF_Fhd_red_mod with (G:=G) (HG:=HG) (R':=Frs HF R) (E':=Frs HF E).
+rewrite Frs_epi. apply incl_refl. apply GF. apply incl_refl.
+rewrite Frs_epi. apply incl_refl. apply GF. apply incl_refl. hyp.
+apply Fhd_red_mod_WF with (S2:=S2) (F:=F) (HF:=HF). hyp.
+Qed.
+
+End Iso.
