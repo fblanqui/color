02b8bd689e8e7c6346b2f9b3b1f4ab0d696c6e6f
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2009-05-20 09:40:25 +0000

    added FS_rtc and prove non-termination instead of non-wellfoundedness

--- trunk/color/NonTermin/ALoop.v
+++ trunk/color/NonTermin/ALoop.v
@@ -4,18 +4,14 @@ See the COPYRIGHTS and LICENSE files.
 
 - Frederic Blanqui & Wang Qian & Zhang Lianyi, 2009-05-11
 
-non-termination from loops
+non-termination from TRS loops
 *)
 
 Set Implicit Arguments.
 
-Require Import NatUtil.
+Require Import LogicUtil.
 Require Import ATrs.
 Require Import ListUtil.
-Require Import ListDec.
-Require Import APosition.
-Require Import LogicUtil.
-Require Import AMatching.
 
 Section S.
 
@@ -51,8 +47,22 @@ Proof.
 intros t x. destruct x. exact f.
 Defined.
 
+Require Import RelUtil.
+
+Lemma FS_rtc : forall us t, FS t us -> red R # t (last us t).
+
+Proof.
+induction us. simpl. intros. apply rt_refl. simpl FS. intuition.
+apply rt_trans with a. apply rt_step. hyp. simpl. destruct us. apply rt_refl.
+rewrite last_default with (d2:=a). apply IHus. hyp. discr.
+Qed.
+
 (***********************************************************************)
 (** data necessary for a sequence of rewrite steps *)
+
+Require Import AMatching.
+Require Import ListDec.
+Require Import APosition.
 
 Definition data := (position * rule)%type.
 
@@ -108,8 +118,10 @@ Qed.
 
 Implicit Arguments rewrites_correct [ds t us].
 
-Lemma FS_red : forall ts t, FS t ts -> forall i,
-  i < length ts -> red R (nth i (t::ts) (Var 0)) (nth (S i) (t::ts) (Var 0)).
+Require Import NatUtil.
+
+Lemma FS_red : forall ts t, FS t ts -> forall i, i < length ts ->
+  red R (nth i (t::ts) (Var 0)) (nth (S i) (t::ts) (Var 0)).
 
 Proof.
 induction ts; simpl; intros. absurd_arith. destruct H. destruct i. hyp.
@@ -165,10 +177,7 @@ Qed.
 Lemma red_g : forall a b, red R a b -> red R (g a) (g b).
 
 Proof.
-intros. redtac. unfold red. unfold g.
-exists l. exists r. exists (comp c (subc s c0)). exists (sub_comp s s0).
-subst. repeat rewrite sub_fill. repeat rewrite fill_fill.
-repeat rewrite sub_sub. intuition.
+intros. unfold g. apply red_fill. apply red_sub. hyp.
 Qed.
 
 Lemma red_iter_g : forall a b, red R a b ->
@@ -181,7 +190,7 @@ Qed.
 
 Require Import Euclid.
 
-Definition f (n : nat) : term.
+Definition seq (n : nat) : term.
 
 Proof.
 intro n. destruct (eucl_dev k h0 n). exact (iter g q (nth r)).
@@ -190,37 +199,30 @@ Defined.
 Require Import RelUtil.
 Require Import Wf_nat.
 
-Lemma IS_f : IS (red R) f.
+Lemma IS_seq : IS (red R) seq.
 
 Proof.
-intro n; pattern n; apply lt_wf_ind; clear n; intros. unfold f at -2 .
+intro n; pattern n; apply lt_wf_ind; clear n; intros. unfold seq at -2 .
 destruct (eucl_dev k h0 n); simpl. destruct (le_gt_dec (k-1) r).
 (* r = k-1 *)
 assert (r = k-1). omega. assert (S n = (S q)*k + 0). rewrite mult_succ_l.
-omega. rewrite H1. unfold f. destruct (eucl_dev k h0 (S q * k + 0)).
+omega. rewrite H1. unfold seq. destruct (eucl_dev k h0 (S q * k + 0)).
 destruct (eucl_div_unique h0 g1 e0). rewrite <- H3. rewrite <- H2. simpl.
 apply red_iter_g. rewrite H0. fold last_term. rewrite last_term_g.
 apply red_g. unfold nth.
 change (red R (List.nth 0 (t :: us) (Var 0)) (List.nth 1 (t :: us) (Var 0))).
 apply FS_red. apply (rewrites_correct h1). hyp.
 (* r < k-1 *)
-assert (S n = q*k + S r). omega. rewrite H0. unfold f.
+assert (S n = q*k + S r). omega. rewrite H0. unfold seq.
 destruct (eucl_dev k h0 (q * k + S r)). assert (k>S r). omega.
 destruct (eucl_div_unique H1 g2 e0). rewrite <- H3. rewrite <- H2.
 apply red_iter_g. apply FS_red'. omega.
 Qed.
 
-Require Import IS_NotSN.
-Require Import SN.
-Require Import AReduct.
-
-Variable (h5 : rules_preserv_vars R).
-
-Lemma loop : ~WF (red R).
+Lemma loop : non_terminating (red R).
 
 Proof.
-unfold not. intro. eapply WF_notIS. apply fin_branch. apply h5. hyp.
-apply IS_f.
+exists seq. apply IS_seq.
 Qed.
 
 End loop.
@@ -247,23 +249,26 @@ Definition is_loop t ds p :=
       end
   end.
 
-Lemma is_loop_correct : rules_preserv_vars R ->
-  forall t ds p, is_loop t ds p = true -> ~WF (red R).
+Lemma is_loop_correct : forall t ds p,
+  is_loop t ds p = true -> non_terminating (red R).
 
 Proof.
-intros HR t ds p. unfold is_loop. coq_case_eq (rewrites t ds). 2: discr.
+intros t ds p. unfold is_loop. coq_case_eq (rewrites t ds). 2: discr.
 destruct l. discr. set (us := t0::l). set (u := last us (Var 0)).
 coq_case_eq (subterm_pos u p). 2: discr. intro v. case_eq (matches t v).
-2: discr. eapply loop. apply H1. unfold k, us. simpl. omega.
-unfold last_term, k, nth. rewrite <- last_nth. fold u. apply H0. apply H. hyp.
+2: discr. assert (h0 : k us > 0). unfold k, us. simpl. omega.
+assert (h : u = last_term us). unfold last_term, k, nth. rewrite <- last_nth.
+refl. rewrite h in H0. exists (seq us h0 p H0 t1). eapply IS_seq. apply H1.
+hyp.
 Qed.
 
 End S.
+
+Implicit Arguments rewrite_correct [Sig R t d u].
+Implicit Arguments rewrites_correct [Sig R ds t us].
 
 (***********************************************************************)
 (** tactics *)
 
 Ltac loop t' ds' p' :=
-  rewrite red_mod_empty;
-  apply is_loop_correct with (t:=t') (ds:=ds') (p:=p');
-    [rules_preserv_vars | vm_compute; refl].
+  apply is_loop_correct with (t:=t') (ds:=ds') (p:=p'); vm_compute; refl.
