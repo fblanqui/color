8ccea04f0a597ff112e28d0d823f6700ddc2d44b
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2009-07-15 10:26:54 +0000

    use Include instead of [Declare] Module, added enumeration of labelled symbols, and added signature module in [Fin]SemLabProps

--- trunk/color/SemLab/ARootLab.v
+++ trunk/color/SemLab/ARootLab.v
@@ -45,70 +45,75 @@ Module RootSemLab (Import R : RootLab) <: FinSemLab.
   Notation term := (term Sig). Notation terms := (vector term).
   Notation rule := (rule Sig). Notation rules := (rules Sig).
 
-  Module SL <: SemLab.
+  Definition Sig := Sig.
 
-    Definition Sig := Sig.
+  Definition I := mkInterpretation some_symbol (fun f _ => f).
 
-    Definition I := mkInterpretation some_symbol (fun f _ => f).
+  Record Lab : Type := mk {
+    L_symb : Sig;
+    L_args : vector I (arity L_symb)
+  }.
 
-    Notation eqI := (IR I (@eq I)). Infix "=I" := eqI (at level 70).
+  Ltac Leqtac := repeat
+    match goal with
+      | H : mk ?x ?v = mk ?x ?w |- _ =>
+        let h := fresh in
+          (injection H; intro h1; ded (inj_pairT2 eq_symb_dec h1);
+            clear h1; clear H)
+      | H : mk ?x ?v = mk ?y ?w |- _ =>
+        let h1 := fresh in let h2 := fresh in
+          (injection H; intros h1 h2; subst; ded (inj_pairT2 eq_symb_dec h1);
+            clear h1; clear H)
+    end.
 
-    Definition beqI (t u : term) :=
-      match t, u with
-        | Var x, Var y => beq_nat x y
-        | Fun f _, Fun g _ => beq_symb f g
-        | _, _ => false
-      end.
+  Definition L := Lab.
 
-    Lemma beqI_ok : rel beqI << eqI.
+  Definition beq (l1 l2 : L) :=
+    let (f1,v1) := l1 in let (f2,v2) := l2 in
+      beq_symb f1 f2 && beq_vec (@beq_symb Sig) v1 v2.
 
-    Proof.
-      intros t u. unfold rel. destruct t; destruct u; simpl; intros; try discr.
-      rewrite beq_nat_ok in H. subst. intro. refl.
-      rewrite beq_symb_ok in H. subst. intro. refl.
-    Qed.
+  Lemma beq_ok : forall l1 l2, beq l1 l2 = true <-> l1 = l2.
 
-    Record Lab : Type := mk {
-      L_symb : Sig;
-      L_args : vector I (arity L_symb)
-    }.
+  Proof.
+    intros [f1 v1] [f2 v2]. simpl. rewrite andb_eq. rewrite beq_symb_ok.
+    intuition. subst. apply beq_vec_beq_impl_eq in H1. subst. refl.
+    apply beq_symb_ok. Leqtac. refl. Leqtac. apply beq_vec_ok2.
+    apply beq_symb_ok. hyp.
+  Qed.
 
-    Ltac Leqtac := repeat
-      match goal with
-        | H : mk ?x ?v = mk ?x ?w |- _ =>
-          let h := fresh in
-            (injection H; intro h1; ded (inj_pairT2 eq_symb_dec h1);
-              clear h1; clear H)
-        | H : mk ?x ?v = mk ?y ?w |- _ =>
-          let h1 := fresh in let h2 := fresh in
-            (injection H; intros h1 h2; subst; ded (inj_pairT2 eq_symb_dec h1);
-              clear h1; clear H)
-      end.
+  Definition pi := mk.
 
-    Definition L := Lab.
+  Definition beqI (t u : term) :=
+    match t, u with
+      | Var x, Var y => beq_nat x y
+      | Fun f _, Fun g _ => beq_symb f g
+      | _, _ => false
+    end.
 
-    Definition beq (l1 l2 : L) :=
-      let (f1,v1) := l1 in let (f2,v2) := l2 in
-        beq_symb f1 f2 && beq_vec (@beq_symb Sig) v1 v2.
+  Lemma beqI_ok : rel beqI << IR I (@eq I).
 
-    Lemma beq_ok : forall l1 l2, beq l1 l2 = true <-> l1 = l2.
-
-    Proof.
-      intros [f1 v1] [f2 v2]. simpl. rewrite andb_eq. rewrite beq_symb_ok.
-      intuition. subst. apply beq_vec_beq_impl_eq in H1. subst. refl.
-      apply beq_symb_ok. Leqtac. refl. Leqtac. apply beq_vec_ok2.
-      apply beq_symb_ok. hyp.
-    Qed.
-
-    Definition pi := mk.
-
-  End SL.
+  Proof.
+    intros t u. unfold rel. destruct t; destruct u; simpl; intros; try discr.
+    rewrite beq_nat_ok in H. subst. intro. refl.
+    rewrite beq_symb_ok in H. subst. intro. refl.
+  Qed.
 
   Definition Fs := Fs.
   Definition Fs_ok := Fs_ok.
 
   Definition Is := Fs.
   Definition Is_ok := Fs_ok.
+
+  Definition Ls := flat_map (fun f =>
+    map (fun fs => @mk f fs) (enum_tuple I Is (arity f))) Fs.
+
+  Lemma Ls_ok : forall x, In x Ls.
+
+  Proof.
+    intros [f fs]. unfold Ls. rewrite in_flat_map. exists f. split.
+    apply Fs_ok. rewrite in_map_iff. exists fs. intuition.
+    apply enum_tuple_complete. apply Is_ok.
+  Qed.
 
 End RootSemLab.
 
