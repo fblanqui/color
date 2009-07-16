e990fbb847944befa366a6568ea06c8d3a37eca6
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2009-07-16 05:29:18 +0000

    added single and add written ::, declared :: as morphim for [= and [=], and added image and lemmas about image

--- Util/Set/SetUtil.v
+++ Util/Set/SetUtil.v
@@ -27,11 +27,16 @@ Definition empty : set := fun _ => False.
 
 Definition union (R S : set) : set := fun x : A => R x \/ S x.
 
+Definition single a : set := fun x : A => x = a.
+
+Definition add a R := union (single a) R.
+
 End defs.
 
 Infix "[=" := incl (at level 70).
 Infix "[=]" := equiv (at level 70).
 Infix "++" := union (right associativity, at level 60).
+Infix "::" := add (at level 60, right associativity).
 
 (***********************************************************************)
 (** inclusion *)
@@ -146,3 +151,47 @@ firstorder.
 Qed.
 
 End union.
+
+(***********************************************************************)
+(** add *)
+
+Add Parametric Morphism (A : Type) : (@add A)
+  with signature (@eq A) ==> (@incl A) ==> (@incl A)
+    as add_incl.
+
+Proof.
+firstorder.
+Qed.
+
+Add Parametric Morphism (A : Type) : (@add A)
+  with signature (@eq A) ==> (@equiv A) ==> (@equiv A)
+    as add_equiv.
+
+Proof.
+firstorder.
+Qed.
+
+(***********************************************************************)
+(** image *)
+
+Section image.
+
+Variables (A B : Type) (f : A -> B).
+
+Definition image (R : set A) : set B := fun b : B => exists a, R a /\ b = f a.
+
+Definition image_union : forall R S, image (R ++ S) [=] image R ++ image S.
+
+Proof.
+firstorder.
+Qed.
+
+Definition image_add : forall a R, image (a :: R) [=] f a :: image R.
+
+Proof.
+intros. unfold add. rewrite image_union. apply equiv_app.
+unfold image, single. split; intro. do 2 destruct H. subst. refl.
+subst. exists a. auto. refl.
+Qed.
+
+End image.
