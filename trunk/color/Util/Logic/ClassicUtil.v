e73b11070a0b6d4f2915b7c976e5aaa913dbc9c5
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2010-04-06 10:29:14 +0000

    added classic equivalences to use with rewrite

--- trunk/color/Util/Logic/ClassicUtil.v
+++ trunk/color/Util/Logic/ClassicUtil.v
@@ -10,6 +10,8 @@ general lemmas in classical logic
 Set Implicit Arguments.
 
 Require Export Classical.
+Require Import LogicUtil.
+Require Import Setoid.
 
 (***********************************************************************)
 (** basic meta-theorems *)
@@ -34,3 +36,43 @@ Qed.
 End meta.
 
 Implicit Arguments not_forall_imply_exists_not [A P].
+
+(***********************************************************************)
+(** basic equivalences *)
+
+Section equiv.
+
+Lemma imply_eq : forall P Q : Prop, (P -> Q) <-> (~P \/ Q).
+
+Proof.
+split; intro. destruct (classic P). right. auto. left. hyp.
+intro. destruct H. absurd P; hyp. hyp.
+Qed.
+
+Lemma not_not_eq : forall P : Prop, ~~P <-> P.
+
+Proof.
+split. apply NNPP. intros h1 h2. absurd P; hyp.
+Qed.
+
+Lemma not_and_eq : forall P Q : Prop, ~(P /\ Q) <-> ~P \/ ~Q.
+
+Proof.
+split; intro. destruct (classic P). right. intro. absurd (P/\Q). hyp. auto.
+left. auto. intros [h1 h2]. destruct H. absurd P; hyp. absurd Q; hyp.
+Qed.
+
+Lemma not_or_eq : forall P Q : Prop, ~(P \/ Q) <-> ~P /\ ~Q.
+
+Proof.
+split. intro. split; intro; absurd (P\/Q); auto.
+intros [h1 h2] [h|h]. absurd P; hyp. absurd Q; hyp.
+Qed.
+
+Lemma not_imply_eq : forall P Q : Prop, ~(P -> Q) <-> P /\ ~Q.
+
+Proof.
+intros. rewrite imply_eq. rewrite not_or_eq. rewrite not_not_eq. refl.
+Qed.
+
+End equiv.
