52529b94608320fe3d676d5e91b1b2a2e962ec3a
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2011-01-27 10:37:05 +0000

    added XOrdProps and removed fold_mon

--- Util/FMap/FMapUtil.v
+++ Util/FMap/FMapUtil.v
@@ -14,29 +14,10 @@ Require Import LogicUtil FMaps FMapAVL FMapFacts.
 Module Make (X : OrderedType).
 
   Module Export XMap := FMapAVL.Make X.
-  Module Export XMapProp := Properties XMap.
+  Module Export XMapProps := Properties XMap.
   Module Export XMapFacts := Facts XMap.
+  Module Export XOrdProps := OrdProperties XMap.
 
   Import X.
-
-(***********************************************************************)
-(* monotony properties of fold *)
-
-Section fold_mon.
-
-  Variables (elt A : Type) (le : A -> A -> Prop).
-
-  Infix "<=" := le.
-
-  Variables (F : key -> elt -> A -> A)
-    (Fmon : forall k x a b, a <= b -> a <= F k x b).
-
-  Lemma fold_mon : forall m a b, a <= b -> a <= fold F m b.
-
-  Proof.
-  intros m a b. apply fold_rec; intros. hyp. apply Fmon. auto.
-  Qed.
-
-End fold_mon.
 
 End Make.
