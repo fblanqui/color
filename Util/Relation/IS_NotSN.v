31da9e7942e98cb48c70d5e8e15cdfdca97a51f6
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2011-05-20 04:28:37 +0000

    prove WF_notIS from the axiom SN_notNT stating the equivalence between SN and ~NT

--- Util/Relation/IS_NotSN.v
+++ Util/Relation/IS_NotSN.v
@@ -4,20 +4,36 @@ See the COPYRIGHTS and LICENSE files.
 
 - Frederic Blanqui, 2011-01-17
 
-Axiom stating the equivalence between WF and ~IS.
+Axiom stating the equivalence between SN and ~NT.
 
 Note however that:
 
 - In IS_NotSN_FB, it is proved that, if R is finitely branching, then:
-  WF R -> (forall f, ~IS R f)
+    WF R -> (forall f, ~IS R f)
+
+We could use this for proving:
+  SN R x -> ~NT R x
+
+by taking the set of reducts of x for A.
 
 - In NotSN_IS, it is proved under the axiom of dependent choice
 (DepChoice) and using classical logic that:
-  (forall f, ~IS R f) -> WF R
+  SN R x <- ~NT R x
+    WF R <- (forall f, ~IS R f)
 *)
 
 Set Implicit Arguments.
 
 Require Import RelUtil SN.
 
-Axiom WF_notIS : forall A (R : relation A), WF R <-> forall f, ~IS R f.
+Axiom SN_notNT : forall A (R : relation A) x, SN R x <-> ~NT R x.
+
+Require Import LogicUtil.
+
+Lemma WF_notIS : forall A (R : relation A), WF R <-> forall f, ~IS R f.
+
+Proof.
+intros A R. split.
+intros wf f hf. ded (wf (f 0)). rewrite SN_notNT in H. apply H. exists f. auto.
+intros h x. rewrite SN_notNT. intros [f [h0 hf]]. eapply h. apply hf.
+Qed.
