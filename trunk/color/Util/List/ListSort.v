83730ca88a599e960e9ed623c44b4cecce29734f
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2011-05-25 02:37:11 +0000

    indentation

--- trunk/color/Util/List/ListSort.v
+++ trunk/color/Util/List/ListSort.v
@@ -13,23 +13,22 @@ Require Import List.
 
 Section S.
 
-Variable A : Type.
-Variable cmp : A -> A -> comparison.
+  Variables (A : Type) (cmp : A -> A -> comparison).
 
-Fixpoint insert x (l : list A) :=
-  match l with
-    | nil => cons x nil
-    | cons y m =>
-      match cmp x y with
-        | Gt => cons y (insert x m)
-        | _ => cons x l
-      end
-  end.
+  Fixpoint insert x (l : list A) :=
+    match l with
+      | nil => cons x nil
+      | cons y m =>
+        match cmp x y with
+          | Gt => cons y (insert x m)
+          | _ => cons x l
+        end
+    end.
 
-Fixpoint sort (l : list A) : list A :=
-  match l with
-    | nil => nil
-    | cons x m => insert x (sort m)
-  end.
+  Fixpoint sort (l : list A) : list A :=
+    match l with
+      | nil => nil
+      | cons x m => insert x (sort m)
+    end.
 
 End S.
