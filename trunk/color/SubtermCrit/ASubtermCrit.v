34bd8775d6bf2f4434272ffe44e379272be410a4
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2011-05-27 09:29:46 +0000

    named assumptions in proofs

--- trunk/color/SubtermCrit/ASubtermCrit.v
+++ trunk/color/SubtermCrit/ASubtermCrit.v
@@ -22,7 +22,8 @@ Section S.
 
   Variable Sig : Signature.
 
-  Notation supterm := (@supterm Sig). Notation supterm_eq := (@supterm_eq Sig).
+  Notation supterm := (@supterm Sig).
+  Notation supterm_eq := (@supterm_eq Sig).
 
 (***********************************************************************)
 (** assumptions *)
@@ -30,8 +31,9 @@ Section S.
   Variables (pi : Sig -> nat) (Hpi : valid pi).
 
   Notation proj := (proj Hpi).
-  Notation ge := (proj_ord Hpi supterm_eq).
-  Notation gt := (proj_ord Hpi supterm).
+
+  Definition ge := proj_ord Hpi supterm_eq.
+  Definition gt := proj_ord Hpi supterm.
 
   Section prop.
 
@@ -50,7 +52,7 @@ Section S.
     Lemma supterm_proj : forall t u s, gt t u -> gt (sub s t) (sub s u).
 
     Proof.
-      intros t u s. unfold proj_ord, transp.
+      intros t u s. unfold gt, proj_ord, transp.
       case t. simpl. intro. case u. simpl. intros.
       destruct H. destruct H. destruct (var_eq_fill H0). tauto.
       intros. rewrite proj_sub_fun.
@@ -66,7 +68,7 @@ Section S.
       In (mkRule l r) D -> ge (sub s l) (sub s r).
 
     Proof.
-      intros l r s Dlr. generalize (hyp1 _ _ Dlr); intro.
+      intros l r s Dlr. unfold ge. generalize (hyp1 _ _ Dlr); intro.
       case_eq (beq_term (proj l) (proj r)). Focus 2.
       assert (proj l <> proj r).
       intro. rewrite (proj2 (beq_term_ok _ _) H1) in H0. discr.
@@ -168,68 +170,83 @@ Section S.
 (***********************************************************************)
 (** WARNING: we use the following axiom *)
 
-Axiom WF_IS_DP : forall Sig (M D : rules Sig), D [= dp M -> 
+Axiom WF_IS_DP : forall Sig (M D : rules Sig), D [= dp M ->
   ~WF (hd_red_mod M D) -> exists f, exists g, ISModMin M D f g.
 
 (***********************************************************************)
 (** theorem with boolean conditions *)
 
-  Notation bge := (fun x y => bsupterm_eq (proj x) (proj y)).
-  Notation bgt := (fun x y => bsupterm (proj x) (proj y)).
+  Definition bge x y := bsupterm_eq (proj x) (proj y).
+  Definition bgt x y := bsupterm (proj x) (proj y).
+
+  (*REMOVE?
+  Lemma bge_ok : forall x y, bge x y = true <-> ge x y.
+
+  Proof.
+    intros x y. apply bsubterm_eq_ok.
+  Qed.
+
+  Lemma bgt_ok : forall x y, bgt x y = true <-> gt x y.
+
+  Proof.
+    intros x y. apply bsubterm_ok.
+  Qed.*)
+
+  Require Import IS_NotSN.
 
   Lemma subterm_criterion_WF : forall M D1 D2,
     D1 ++ D2 [= dp M ->
-      forallb (@is_notvar_lhs Sig) (D1 ++ D2) = true ->
       forallb (brule bge) (D1 ++ D2) = true ->
-      forallb (brule bgt) D1 = true -> 
+      forallb (brule bgt) D1 = true ->
+      forallb (@is_notvar_lhs Sig) (D1 ++ D2) = true -> 
       WF (hd_red_mod M D2) -> WF (hd_red_mod M (D1 ++ D2)).
 
   Proof.
-    intros. induction D1; simpl. hyp.
-    apply NNPP. intro H4. destruct (WF_IS_DP H H4) as [f [g H5]].
-    assert (H6 : forall l r, In (mkRule l r) (a :: D1 ++ D2) -> ge l r).
-    intros. generalize (proj1 (forallb_forall _ _) H1 (mkRule l r) H6).
+    intros M D1 D2 H b1 b2 b3 h. destruct D1 as [|a D1]. auto.
+    assert (h1 : forall l r, In (mkRule l r) (a :: D1 ++ D2) -> ge l r).
+    intros l r lr. generalize (proj1 (forallb_forall _ _) b1 _ lr).
     unfold brule. simpl. apply (proj1 (bsubterm_eq_ok _ _)).
-    assert (H7 : exists l, exists r, In (mkRule l r) (a :: D1 ++ D2) /\ gt l r).
+    assert (h2 : exists l, exists r, In (mkRule l r) (a :: D1 ++ D2) /\ gt l r).
     exists (lhs a). exists (rhs a). simpl. split.
     left. apply (proj1 (rule_eq _ _)). simpl. auto.
-    generalize (proj1 (forallb_forall _ _) H2 a (in_eq a _)). unfold brule.
+    generalize (proj1 (forallb_forall _ _) b2 a (in_eq a _)). unfold brule.
     apply (proj1 (bsubterm_ok _ _)).
-    assert (H8 : forall l r, In (mkRule l r) (a :: D1 ++ D2) ->
+    assert (h3 : forall l r, In (mkRule l r) (a :: D1 ++ D2) ->
       is_notvar_lhs (mkRule l r) = true).
-    intros. apply (proj1 (forallb_forall _ _) H0). auto.
-    apply (@subterm_criterion_IS M (a :: D1 ++ D2) H6 H7 H8 f g). hyp.
+    intros. apply (proj1 (forallb_forall _ _) b3). auto.
+    ded (@subterm_criterion_IS M _ h1 h2 h3). clear -H0 H.
+    apply NNPP. intro h. destruct (WF_IS_DP H h) as [f [g hfg]].
+    ded (H0 f g). contr.
   Qed.
 
-  Lemma subterm_criterion : forall M D,
-    D [= dp M ->
-      forallb (@is_notvar_lhs Sig) D = true ->
-      forallb (brule bge) D = true ->
-      WF (hd_red_mod M (filter (brule (neg bgt)) D)) ->
-      WF (hd_red_mod M D).
+  Lemma subterm_criterion : forall M D, D [= dp M ->
+    forallb (brule bge) D = true ->
+    forallb (@is_notvar_lhs Sig) D = true ->
+    WF (hd_red_mod M (filter (brule (neg bgt)) D)) ->
+    WF (hd_red_mod M D).
 
   Proof.
-    intros. pose (D' := filter (brule (neg bgt)) D).
-    pose (D0 := filter (brule bgt) D).
-    assert (HD : (hd_red_mod M D) << (hd_red_mod M (D0 ++ D'))).
-    apply hd_red_Mod_incl. refl. intros d Dd. apply in_or_app.
+    intros M D H b2 b3. set (De := filter (brule (neg bgt)) D).
+    pose (Dt := filter (brule bgt) D).
+    assert (i : hd_red_mod M D << hd_red_mod M (Dt ++ De)).
+    apply hd_red_Mod_incl. refl. intros d hd. apply in_or_app.
     case_eq (brule (neg bgt) d). right. apply (proj2 (filter_In _ _ _)).
     intuition.
-    left. apply (proj2 (filter_In _ _ _)). split; try hyp. unfold brule in H3.
-    generalize (proj1 (negb_lr _ _) H3). simpl. auto.
-    apply (WF_incl HD). apply subterm_criterion_WF; auto.
-    intro; rewrite in_app; unfold D0, D'; intro T. destruct T as [T|T]; apply H;
-    rewrite filter_In in T; destruct T; auto.
-    apply (proj2 (forallb_forall _ _)). intros.
-    apply (proj1 (forallb_forall _ _) H0 x). destruct (in_app_or H3).
-    unfold D0 in H4. rewrite filter_In in H4. destruct H4. hyp.
-    unfold D' in H4. rewrite filter_In in H4. destruct H4. hyp.
-    apply (proj2 (forallb_forall _ _)). intros.
-    apply (proj1 (forallb_forall _ _) H1 x). destruct (in_app_or H3).
-    unfold D0 in H4. rewrite filter_In in H4. destruct H4. hyp.
-    unfold D' in H4. rewrite filter_In in H4. destruct H4. hyp.
-    apply (proj2 (forallb_forall _ _)). intros. unfold D0 in H3.
-    rewrite filter_In in H3. intuition.
+    left. apply (proj2 (filter_In _ _ _)). split; try hyp. unfold brule in H0.
+    generalize (proj1 (negb_lr _ _) H0). auto.
+    intro he. apply (WF_incl i). apply subterm_criterion_WF; auto.
+    intro; rewrite in_app; unfold Dt, De; intro T.
+    destruct T as [T|T]; apply H; rewrite filter_In in T; destruct T; auto.
+    apply (proj2 (forallb_forall _ _)). intros x hx.
+    apply (proj1 (forallb_forall _ _) b2 x). destruct (in_app_or hx) as [h|h].
+    unfold Dt in h. rewrite filter_In in h. destruct h. hyp.
+    unfold De in h. rewrite filter_In in h. destruct h. hyp.
+    apply (proj2 (forallb_forall _ _)). intros x hx.
+    unfold Dt in hx. rewrite filter_In in hx. destruct hx. hyp.
+    apply (proj2 (forallb_forall _ _)). intros x hx.
+    apply (proj1 (forallb_forall _ _) b3 x). destruct (in_app_or hx) as [h|h].
+    unfold Dt in h. rewrite filter_In in h. destruct h. hyp.
+    unfold De in h. rewrite filter_In in h. destruct h. hyp.
   Qed.
 
 End S.
