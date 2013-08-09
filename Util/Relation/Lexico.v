5c8207195ab5c0355cbe924f1bcf69a630377320
Frédéric Blanqui <fblanqui@users.noreply.github.com>
2013-08-09 07:52:21 +0000

    moved lex and lex' from Lexico to Union (and renamed them R1 and R1' respectively); renamed lexp into lex; and added lexn and lexv

--- Util/Relation/Lexico.v
+++ Util/Relation/Lexico.v
@@ -9,12 +9,12 @@ lexicographic ordering
 
 Set Implicit Arguments.
 
-Require Import SN RelUtil LogicUtil.
+Require Import SN RelUtil LogicUtil Morphisms.
 
 (****************************************************************************)
 (** lexicographic quasi-ordering on pairs *)
 
-Section lexp.
+Section lex.
 
   Variable (A : Type) (gtA eqA : relation A) (Hcomp : eqA @ gtA << gtA).
 
@@ -27,33 +27,33 @@ Section lexp.
 
   Variable (B : Type) (gtB : relation B).
 
-  Inductive lexp : relation (prod A B) :=
-  | lexp1 : forall a a' b b', gtA a a' -> lexp (a,b) (a',b')
-  | lexp2 : forall a a' b b', eqA a a' -> gtB b b' -> lexp (a,b) (a',b').
+  Inductive lex : relation (prod A B) :=
+  | lex1 : forall a a' b b', gtA a a' -> lex (a,b) (a',b')
+  | lex2 : forall a a' b b', eqA a a' -> gtB b b' -> lex (a,b) (a',b').
 
-  Lemma lexp_intro : forall a a' b b',
-    gtA a a' \/ (eqA a a' /\ gtB b b') -> lexp (a,b) (a',b').
+  Lemma lex_intro : forall a a' b b',
+    gtA a a' \/ (eqA a a' /\ gtB b b') -> lex (a,b) (a',b').
 
   Proof.
-    intros. destruct H. apply lexp1. exact H. destruct H. apply lexp2.
+    intros. destruct H. apply lex1. exact H. destruct H. apply lex2.
     exact H. exact H0.
   Qed.
 
-  Variable (WF_gtB : WF gtB) (eqA_trans : transitive eqA).
+  Variable (WF_gtB : WF gtB) (eqA_trans : Transitive eqA).
 
-  Lemma lexp_SN_eq : forall a b,
-    SN lexp (a,b) -> forall a', eqA a a' -> SN lexp (a',b).
+  Lemma lex_SN_eq : forall a b,
+    SN lex (a,b) -> forall a', eqA a a' -> SN lex (a',b).
 
   Proof.
     intros a b SN_ab a' eqaa'. inversion SN_ab. apply SN_intro.
     destruct y as (a'',b'). intro H'.
     inversion H'; subst a'0 b'0 a0 b0; apply H.
-    apply lexp1. apply (inclusion_elim Hcomp). exists a'. auto.
-    apply lexp2. apply (eqA_trans eqaa' H3). exact H5.
+    apply lex1. apply (inclusion_elim Hcomp). exists a'. auto.
+    apply lex2. apply (eqA_trans eqaa' H3). exact H5.
   Qed.
 
-  Lemma lexp_SN :
-    forall a, SN gtA a -> forall b, SN gtB b -> SN lexp (a, b).
+  Lemma lex_SN :
+    forall a, SN gtA a -> forall b, SN gtB b -> SN lex (a, b).
 
   Proof.
     induction 1 as [a Ha1 Ha2]. induction 1 as [b Hb1 Hb2]. apply SN_intro.
@@ -61,55 +61,56 @@ Section lexp.
     (* gtA a a' *)
     apply Ha2. exact H1. apply WF_gtB.
     (* eqA a a' /\ gtB b b' *)
-    apply (@lexp_SN_eq a). 2: exact H3. apply Hb2. exact H5.
+    apply (@lex_SN_eq a). 2: exact H3. apply Hb2. exact H5.
   Qed.
 
   Variable WF_gtA : WF gtA.
 
-  Lemma WF_lexp : WF lexp.
-
-  Proof.
-    unfold WF. destruct x as (a,b). apply lexp_SN. apply WF_gtA. apply WF_gtB.
-  Qed.
-
-End lexp.
-
-(****************************************************************************)
-(** lexicographic ordering on the same argument *)
-
-Section lex.
-
-  Variable (A : Type) (gtA eqA gtA' : relation A)
-    (eqA_trans : transitive eqA) (Hcomp : eqA @ gtA << gtA)
-    (WF_gtA : WF gtA) (WF_gtA' : WF gtA').
-
-  Definition lex x y := lexp gtA eqA gtA' (x,x) (y,y).
-
   Lemma WF_lex : WF lex.
 
   Proof.
-    exact
-    (WF_inverse (fun x:A => (x,x)) (WF_lexp Hcomp WF_gtA' eqA_trans WF_gtA)).
+    unfold WF. destruct x as (a,b). apply lex_SN. apply WF_gtA. apply WF_gtB.
   Qed.
 
 End lex.
 
-Section lex'.
+(****************************************************************************)
+(** * Lexicographic order on tuples and vectors. *)
 
-  Variable (A : Type) (gt1 gt2 : relation A)
-    (WF_gt1 : WF gt1) (WF_gt2 : WF gt2)
-    (trans_gt2 : transitive gt2) (Hcomp : gt2 @ gt1 << gt1).
+Section lexn.
 
-  Definition lex' := lex gt1 gt2 gt2.
+  Variables (A : Type) (gtA eqA : relation A) (Hcomp : eqA @ gtA << gtA)
+    (eqA_trans : Transitive eqA) (gtA_wf : WF gtA).
 
-  Lemma WF_lex' : WF lex'.
+  Fixpoint prodn n : Type :=
+    match n with
+      | 0 => unit
+      | S n' => prod A (prodn n')
+    end.
 
-  Proof. unfold lex'. apply WF_lex; hyp. Qed.
+  Fixpoint lexn n :=
+    match n as n return relation (prodn n) with
+      | 0 => empty_rel
+      | S n' => lex gtA eqA (lexn n')
+    end.
 
-  Lemma lex'_intro : gt1 U gt2 << lex'.
+  Lemma lexn_wf n : WF (lexn n).
 
-  Proof.
-    unfold lex', lex, inclusion. intros. apply lexp_intro. destruct H; auto.
-  Qed.
+  Proof. induction n; simpl. apply WF_empty_rel. apply WF_lex; hyp. Qed.
 
-End lex'.
+  Require Import VecUtil.
+
+  Fixpoint prod_of_vec n (v : vector A n) :=
+    match v in vector _ n return prodn n with
+      | Vnil => tt
+      | Vcons x _ v' => (x, prod_of_vec v')
+    end.
+
+  Definition lexv n (v w : vector A n) :=
+    lexn n (prod_of_vec v) (prod_of_vec w).
+
+  Lemma lexv_wf n : WF (@lexv n).
+
+  Proof. apply WF_inverse. apply lexn_wf. Qed.
+
+End lexn.
