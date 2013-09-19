97656088d4b3514dd9cbf5a5ae31bf7beff5eba8
blanqui <blanqui@b0d5273c-1889-4eb6-95f8-211497179ee6>
2013-09-19 12:29:00 +0000

    indentation

--- trunk/color/Util/Relation/OrdDec.v
+++ trunk/color/Util/Relation/OrdDec.v
@@ -13,73 +13,73 @@ Require Import RelUtil LogicUtil.
 
 Section S.
 
-Variable A : Type.
+  Variable A : Type.
 
 (***********************************************************************)
 (** properties *)
 
-Section properties.
+  Section properties.
 
-Variable cmp : A -> A -> comparison.
+    Variable cmp : A -> A -> comparison.
 
-Definition is_correct := forall x y, cmp x y = Eq -> x = y.
+    Definition is_correct := forall x y, cmp x y = Eq -> x = y.
 
-Definition greater x y := cmp x y = Gt.
-Definition smaller x y := cmp x y = Lt.
-Definition equal x y := cmp x y = Eq.
+    Definition greater x y := cmp x y = Gt.
+    Definition smaller x y := cmp x y = Lt.
+    Definition equal x y := cmp x y = Eq.
 
-Definition is_symrefl := forall x y, cmp x y = CompOpp (cmp y x).
+    Definition is_symrefl := forall x y, cmp x y = CompOpp (cmp y x).
 
-Variable hyp_symrefl : is_symrefl.
+    Variable hyp_symrefl : is_symrefl.
 
-Lemma equal_refl : reflexive equal. (* forall x, cmp x x = Eq. *)
+    Lemma equal_refl : reflexive equal. (* forall x, cmp x x = Eq. *)
 
-Proof.
-unfold reflexive, equal. intro. case_eq (cmp x x); intros. refl.
-absurd (cmp x x = Gt). rewrite H. discr. rewrite hyp_symrefl. rewrite H. refl.
-absurd (cmp x x = Lt). rewrite H. discr. rewrite hyp_symrefl. rewrite H. refl.
-Qed.
+    Proof.
+      unfold reflexive, equal. intro. case_eq (cmp x x); intros. refl.
+      absurd (cmp x x = Gt). rewrite H. discr. rewrite hyp_symrefl, H. refl.
+      absurd (cmp x x = Lt). rewrite H. discr. rewrite hyp_symrefl, H. refl.
+    Qed.
 
-Lemma equal_sym : symmetric equal.
+    Lemma equal_sym : symmetric equal.
 
-Proof.
-unfold symmetric, equal. intros. rewrite hyp_symrefl. rewrite H. refl.
-Qed.
+    Proof.
+      unfold symmetric, equal. intros. rewrite hyp_symrefl. rewrite H. refl.
+    Qed.
 
-Lemma greater_antisym : antisymmetric greater.
+    Lemma greater_antisym : antisymmetric greater.
 
-Proof.
-unfold antisymmetric, greater. intros x y h. rewrite hyp_symrefl. rewrite h.
-discr.
-Qed.
+    Proof.
+      unfold antisymmetric, greater. intros x y h. rewrite hyp_symrefl, h.
+      discr.
+    Qed.
 
 (***********************************************************************)
 (** transitivity *)
 
-Definition is_trans :=
-  forall x y z c, cmp x y = c -> cmp y z = c -> cmp x z = c.
+    Definition is_trans :=
+      forall x y z c, cmp x y = c -> cmp y z = c -> cmp x z = c.
 
-Variable hyp_trans : is_trans.
+    Variable hyp_trans : is_trans.
 
-Lemma equal_trans : transitive equal.
+    Lemma equal_trans : transitive equal.
 
-Proof.
-unfold transitive, equal. intros. apply hyp_trans with y; hyp.
-Qed.
+    Proof.
+      unfold transitive, equal. intros. apply hyp_trans with y; hyp.
+    Qed.
 
-Lemma greater_trans : transitive greater.
+    Lemma greater_trans : transitive greater.
 
-Proof.
-unfold transitive, greater. intros. apply hyp_trans with y; hyp.
-Qed.
+    Proof.
+      unfold transitive, greater. intros. apply hyp_trans with y; hyp.
+    Qed.
 
-Lemma smaller_trans : transitive smaller.
+    Lemma smaller_trans : transitive smaller.
 
-Proof.
-unfold transitive, smaller. intros. apply hyp_trans with y; hyp.
-Qed.
+    Proof.
+      unfold transitive, smaller. intros. apply hyp_trans with y; hyp.
+    Qed.
 
-End properties.
+  End properties.
 
 End S.
 
@@ -97,6 +97,8 @@ Record Bool_ord_type : Type := mkBool_ord_type {
 (***********************************************************************)
 (** natural numbers *)
 
+(*FIXME: already defined in Coq as Compare_dec.nat_compare. *)
+
 Fixpoint nat_cmp (x y : nat) : comparison :=
   match x, y with
     | 0, 0 => Eq
@@ -107,20 +109,18 @@ Fixpoint nat_cmp (x y : nat) : comparison :=
 
 Lemma nat_cmp_symrefl : is_symrefl nat_cmp.
 
-Proof.
-unfold is_symrefl. induction x; intros; destruct y; simpl; auto.
-Qed.
+Proof. unfold is_symrefl. induction x; intros; destruct y; simpl; auto. Qed.
 
 Lemma nat_cmp_trans : is_trans nat_cmp.
 
 Proof.
-unfold is_trans. induction x; induction y; simpl; intros; destruct z; simpl;
-  subst; auto; try discr. trans (nat_cmp y z). 2: hyp.
-apply (IHx y z (nat_cmp y z)); auto.
+  unfold is_trans. induction x; induction y; simpl; intros; destruct z; simpl;
+    subst; auto; try discr.
+  trans (nat_cmp y z). 2: hyp. apply (IHx y z (nat_cmp y z)); auto.
 Qed.
 
 Lemma nat_cmp_correct : is_correct nat_cmp.
 
 Proof.
-unfold is_correct. induction x; destruct y; simpl; intros; auto; discr.
+  unfold is_correct. induction x; destruct y; simpl; intros; auto; discr.
 Qed.
